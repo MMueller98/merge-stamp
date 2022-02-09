f141c36fc6c6b1aeefbd5fc75aceaf4bf5371bcc
MMueller98 <82333133+MMueller98@users.noreply.github.com>
2022-02-09 15:22:40 +0100

    bugfix png in din a4-format

--- merge-stamps.bat
+++ merge-stamps.bat
@@ -7,16 +7,19 @@ ECHO "==============================================================="
 ECHO: 
 
 FOR %%i IN (*.pdf) DO (
-	magick convert -verbose -density 150 -trim %%i -quality 100 -flatten -sharpen 0x1.0 -append %%i.jpg
+	magick convert -verbose -density 300x300 -trim %%i -quality 100 -flatten -sharpen 0x1.0 -append %%i.png
 	move %%i ../processed/%%i
 )
 
 ECHO "==============================================================="
-ECHO "Concatenate single stamps into one jpg-file -> stamps.jpg"
+ECHO "Concatenate single stamps into one png-file -> stamps.png"
 ECHO "==============================================================="
 ECHO: 
 
-magick montage -mode concatenate -tile 2x -crop 500x350+0+0 *.jpg ../stamps.jpg
+magick montage -verbose -density 300x300 -mode concatenate -tile 2x -crop 1200x875+0+0 *.png ../stamps.png
 
-DEL *.jpg
+DEL *.png
 cd ..
+
+magick convert stamps.png -resize 1240x1753 -units PixelsPerInch -density 150x150 -page a4 multipage.pdf
+
