1b405bed1e5e094272276b73c95fe79d42a27f5b
MMueller98 <82333133+MMueller98@users.noreply.github.com>
2022-02-22 22:55:42 +0100

    programm is functional now

--- merge-stamps.bat
+++ merge-stamps.bat
@@ -6,9 +6,16 @@ ECHO "Converting .pdf stamps to jpg-files"
 ECHO "==============================================================="
 ECHO: 
 
+SET IMAGECOUNTER=0
+
 FOR %%i IN (*.pdf) DO (
 	magick convert -verbose -density 300x300 -trim %%i -quality 100 -flatten -sharpen 0x1.0 -append %%i.png
 	move %%i ../processed/%%i
+	set /A IMAGECOUNTER=IMAGECOUNTER+1
+)
+
+FOR /l %%j IN (%IMAGECOUNTER%, 1, 9) DO (
+	Copy ..\Installation\white.png "zz%%j.png"
 )
 
 ECHO "==============================================================="
@@ -16,11 +23,10 @@ ECHO "Concatenate single stamps into one png-file -> stamps.png"
 ECHO "==============================================================="
 ECHO: 
 
-magick montage -verbose -density 300x300 -mode concatenate -tile 2x -crop 1200x875+0+0 *.png ../stamps.png
+magick montage -verbose -density 300x300 -mode concatenate -tile 2x -crop 1240x701+0+0 *.png ../stamps.png
 
 DEL *.png
 MOVE *.pdf ../Processed
 cd ..
 
-magick convert stamps.png -resize 1240x1753 -units PixelsPerInch -density 150x150 -page a4 multipage.pdf
-
+magick convert -density 72 -page a4 stamps.png multipage.pdf
